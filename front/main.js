3be75f453fa6332295ec1d079a301a2fd4fb176f
NastyaNasalevich <nastya.nasalevich@gmail.com>
2018-04-22 22:10:34 +0300

    second commit

--- front/main.js
+++ front/main.js
@@ -1,60 +1,86 @@
-var cell = document.getElementsByClassName('cell');
-
-var pressAdd = document.getElementById('add');
-pressAdd.addEventListener('click', function () {
-    var addCell = document.createElement('div');
-    addCell.classList.add('cell');
-    document.getElementById('road').appendChild(addCell);
-    addCell.onclick = addHillock;
-    });
-
-function addHillock(){
-    this.classList.toggle("hillock");
-    };
-
-var pressDel = document.getElementById('delete');
-pressDel.addEventListener('click', function () {
-    deliteCell = document.getElementsByClassName('cell')[cell.length-1];
-    deliteCell.parentNode.removeChild(deliteCell);
-    });
-
-var jump = document.getElementsByClassName('jump');
-    for(var i = 0; i < jump.length; i++){
-    jump[i].onclick = makeJump;
-    }   
-
-var arrStep=[0];
-var number, previousStep;
-function makeJump(){
-    if (arrStep.length==1){
-        number=1;
-        previousStep=0;}
-     else {
-        var dataNumber=this.getAttribute('data-number');
-        number=parseInt(dataNumber);
-        previousStep=arrStep[arrStep.length-1]-arrStep[arrStep.length-2];
-        }
-    var newPosition=arrStep[arrStep.length-1]+previousStep+number;
-        arrStep.push(newPosition);
-        updateButton(previousStep);
-    cell[arrStep[arrStep.length-2]].classList.toggle("student");
-    cell[arrStep[arrStep.length-1]].classList.toggle("student"); 
-} 
-
-function updateButton(previousStep){
-var reducedJump = document.getElementsByClassName('jump')[0];
-    reducedJump.innerText='Make '+(previousStep-1)+' jump';
-var sameJump = document.getElementsByClassName('jump')[1];
-    sameJump.innerText='Make '+(previousStep)+' jump';
-var increasedJump = document.getElementsByClassName('jump')[2];
-    increasedJump.innerText='Make '+(previousStep+1)+' jump';
+let cancelButton = document.getElementById('cancel');
+cancelButton.disabled = true;
+let jumpButtons = document.getElementsByClassName('jump');
+   for (let i = 0; i < jumpButtons.length; i++) {
+   		jumpButtons[i].disabled = i==0 || i==2;
 }
 
-var pressCancel = document.getElementsByClassName('cancel')[0];
-    pressCancel.onclick= cancelJump;  
+let cells = document.getElementsByClassName('cell');
 
-function cancelJump (){
-    var cell = document.getElementsByClassName('cell');
-    cell[arrStep[arrStep.length-1]].classList.toggle("student"); 
-    cell[arrStep[arrStep.length-2]].classList.toggle("student");
+let addButton = document.getElementById('add');
+addButton.addEventListener('click', function () {
+    let newCell = document.createElement('div');
+    newCell.classList.add('cell');
+    document.getElementById('road').appendChild(newCell);
+    newCell.onclick = addHillock;
+
+    setButtonsDisableState();    
+});
+
+function addHillock() {
+    this.classList.toggle("hillock");
+
+    setButtonsDisableState();
+};
+
+let deleteButton = document.getElementById('delete');
+deleteButton.addEventListener('click', function () {    
+    let deleteCell = cells[cells.length - 1]    
+    deleteCell.parentNode.removeChild(deleteCell);
+
+    setButtonsDisableState();
+});
+
+for (let i = 0; i < jumpButtons.length; i++) {
+    jumpButtons[i].onclick = makeJump;
+}
+
+let arrStep = [];
+let currentPosition = 0;
+let previousStep = 0;
+function makeJump() {
+    let dataNumber = this.getAttribute('data-number');
+    let stepOffset = parseInt(dataNumber);
+    let stepSize = stepOffset + previousStep + 1;   
+    let newPosition = stepSize + currentPosition;    
+
+    // сохраняем предыдущую позицию студента
+    arrStep.push(currentPosition);    
+
+    // убираем иконку студента с предыдущей позиции и ставим на новую
+    cells[currentPosition].classList.toggle("student");    
+    cells[newPosition].classList.toggle("student");
+
+    previousStep = newPosition - currentPosition - 1;
+
+    // обновляем позицию
+    currentPosition = newPosition;
+
+    updateStepButtonsLabels();        
+    setButtonsDisableState();
+}
+
+function updateStepButtonsLabels() {
+    for (let i = 0; i < jumpButtons.length; i++) {
+        jumpButtons[i].innerText = 'Make ' + (previousStep + i) + ' jump';
+    }
+}
+
+cancelButton.addEventListener('click', function () {           
+    let previousPosition = arrStep.pop();    
+    cells[currentPosition].classList.toggle("student");
+    cells[previousPosition].classList.toggle("student");
+    currentPosition = previousPosition;   
+
+    updateStepButtonsLabels(currentPosition);    
+    setButtonsDisableState();     
+});
+
+function setButtonsDisableState() {
+    cancelButton.disabled = arrStep.length === 0;
+    let lastCell = cells[cells.length - 1];    
+    deleteButton.disabled = lastCell.classList.contains('student');
+    for (let i = 0; i < jumpButtons.length; i++) {
+   		jumpButtons[i].disabled = ((currentPosition + previousStep + i) > (cells.length - 1)) || (previousStep + i)<1 || (i==2 && arrStep.length==0);
+	}
 }
