7dc2f88f0df553f9a81649722cd4d58dcb5a315f
NastyaNasalevich <nastya.nasalevich@gmail.com>
2018-04-27 10:26:59 +0300

    Third commit

--- Main.java
+++ Main.java
@@ -1,38 +1,99 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
-    public static boolean test(int[] nums) {
-        int previousStep = 1;
-        boolean isJump = false;
+    public static class Steps {
 
-        if (nums[0] == 0 && nums.length >= 2) {
-            for (int i = 0; i < nums.length - 1; i++) {
-                boolean isNext = false;
-                for (int j = 1; !isNext && j < nums.length - i; j++) {
-                    if (nums[i + j] - nums[i] == previousStep || nums[i + j] - nums[i] == previousStep + 1 || nums[i + j] - nums[i] == previousStep - 1) {
-                        previousStep = nums[i + j] - nums[i];
-                        i+= j-1;
-                        isNext = true;
-                    }
-                    else isNext = false;
-                }
-                if (isNext == false) {
-                    isJump = false;
-                    break;
-                }
-                isJump = true;
-            }
+        int meaning;
+        ArrayList<Integer> jumps = new ArrayList<>();
+        boolean isStep = false;
+    }
+
+    public static void fillingIn(int[] nums, ArrayList<Steps> steps) {
+
+        for(int i =0; i<nums.length; i++)
+        {
+            steps.add(new Steps());
+            steps.get(i).meaning = nums[i];
         }
-        System.out.println(isJump);
-        return isJump;
+    }
+
+    public static boolean test(int[] nums, ArrayList<Steps> steps) {
+
+        if (nums.length < 2 || nums[0] != 0) {
+            System.out.println(false);
+
+            return false;
+        }
+        else {
+
+            steps.get(1).jumps.add(1);
+            steps.get(1).isStep = true;
+
+            boolean isTrue = false;
+
+            for (int i = 1; i < nums.length; i++) {
+                for (int j = 0; j < steps.get(i).jumps.size(); j++) {
+
+                    for (int k = i; k < steps.size(); k++) {
+
+                        if (steps.get(i).jumps.get(j) == steps.get(k).meaning - steps.get(i).meaning) {
+                            steps.get(k).isStep = true;
+                            steps.get(k).jumps.add(steps.get(i).jumps.get(j));
+                        }
+                        if (steps.get(i).jumps.get(j) + 1 == steps.get(k).meaning - steps.get(i).meaning) {
+                            steps.get(k).isStep = true;
+                            steps.get(k).jumps.add(steps.get(i).jumps.get(j) + 1);
+                        }
+                        if (steps.get(i).jumps.get(j) - 1 == steps.get(k).meaning - steps.get(i).meaning && steps.get(i).jumps.get(j) - 1 != 0) {
+                            steps.get(k).isStep = true;
+                            steps.get(k).jumps.add(steps.get(i).jumps.get(j) - 1);
+                        }
+                    }
+                }
+            }
+            if (steps.get(nums.length - 1).isStep == true) {
+                isTrue = true;
+            }
+
+            System.out.println(isTrue);
+
+            return isTrue;
+        }
     }
 
     public static void main(String[] args) {
-        test(new int[]{0, 1, 3, 5, 8, 12, 17});
-        test(new int[]{0, 1, 2, 3, 4, 9, 11});
-        test(new int[]{0});
-        test(new int[]{1});
-        test(new int[]{0, 1, 3, 5, -1, 8, 12, 17});
+
+        ArrayList<Steps> steps = new ArrayList<>();
+
+        fillingIn(new int[]{0, 1, 3, 5, 6, 8, 12, 17}, steps);
+        test(new int[]{0, 1, 3, 5, 6, 8, 12, 17}, steps);
+        steps.clear();
+
+        fillingIn(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15}, steps);
+        test(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10, 15}, steps);
+        steps.clear();
+
+        fillingIn(new int[]{0, 1, 2, 3, 4, 8, 9, 11}, steps);
+        test(new int[]{0, 1, 2, 3, 4, 8, 9, 11}, steps);
+        steps.clear();
+
+        fillingIn(new int[]{1}, steps);
+        test(new int[]{1}, steps);
+        steps.clear();
+
+        fillingIn(new int[]{0}, steps);
+        test(new int[]{0}, steps);
+        steps.clear();
+
+        fillingIn(new int[]{1, 2}, steps);
+        test(new int[]{1, 2}, steps);
+        steps.clear();
+
+        fillingIn(new int[]{0, 1, 3, 5, -1, 8, 12, 17}, steps);
+        test(new int[]{0, 1, 3, 5, -1, 8, 12, 17}, steps);
+        steps.clear();
     }
 }
